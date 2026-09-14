f4d74ae4205813fb4ff2b91640d7dca331692f33
Vheoa <stdio.conio999@gmail.com>
2026-09-14 17:19:20 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const { https://sqeoddatsnodwxljlyer.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZW9kZGF0c25vZHd4bGpseWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzNzkzNzIsImV4cCI6MjEwNDk1NTM3Mn0.NHKEiqo16zp0UPU8dZeghRj-2-YVZ-uMakvE2Tjeogg } = window.VHEOA;
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = window.VHEOA;
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false },       // no login in Phase 0–3
